9839d036281e822cbcb97c5781adf99052182e7b
Francesc Obrador <obrador.francesc@gmail.com>
2025-01-17 16:37:00 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyectos-DIRI/sesion-5/memo/', 
   build: {
     outDir: 'docs', // Para Github Pages
   }
