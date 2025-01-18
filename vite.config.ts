19ed07d380acdabb87110a095d946433a311f78a
Francesc Obrador <obrador.francesc@gmail.com>
2025-01-18 11:12:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/memo/',
+  base: 'https://francescobrador.github.io/memo/',
   build: {
     outDir: 'docs', // Para Github Pages
   }
